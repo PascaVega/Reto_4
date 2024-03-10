b43745f74fe07ce220a23de771367d6dc8fb3d69
PascaVega <159050569+PascaVega@users.noreply.github.com>
2024-03-10 14:49:02 -0500

    Update #RETO 4 - Parte 3.py

--- #RETO 4 - Parte 3.py
+++ #RETO 4 - Parte 3.py
@@ -2,28 +2,31 @@
 #Dado un carácter, construya un programa en Python para determinar si el caracter es un dígito o no.
 
 def introducir():
-    caracter = input("Ingrese un caracter: ")
+    caracter : str = input("Ingrese un caracter: ")
     determinar(caracter)
     return
 
 def determinar(caracter):
-    num_caracter = ord(caracter)
+    num_caracter : int = ord(caracter)
     if 128 > num_caracter:
         print(f"El caracter {caracter} está en la ISCII")
     else:
         print(f"El caracter {caracter} no está en la ISCII")
     
-
 def continuar():
-    opcion = int(input("¿Desea continuar? Marque 1 (sí) o 2 (no): "))
+    opcion : int = int(input("¿Desea continuar? Marque 1 (sí) o 2 (no): "))
     return opcion
 
-print("Programa para determinar si un caracter es un digito o no")
+if __name__ == "__main__":
+    print("Programa para determinar si un caracter es un digito o no")
 
-while True:
-    introducir()
-    opcion = continuar()
-    if opcion == 2:
-        break  
+    while True:
+        introducir()
+        opcion = continuar()
+        if opcion == 2:
+            break
+        elif opcion != 1 and 2:
+            print("Sintax error")
+            break
 
 # ! /\|=\/
